b8417122135c369e0c7108f6e14877551480c95c
Leon Ludwig <leon.ludwig@ludwig-software.com>
2021-07-08 11:43:44 +0200

    added documentation

--- src/LUAPI/OAS3/OAS3Validator.php
+++ src/LUAPI/OAS3/OAS3Validator.php
@@ -9,16 +9,36 @@ use League\OpenAPIValidation\Schema\Exception;
 use League\OpenAPIValidation\Schema\Exception\KeywordMismatch;
 use League\OpenAPIValidation\Schema\Exception\SchemaMismatch;
 
+/**
+ * a class that can be used to validate input parameters against an oas3 schema
+ */
 abstract class OAS3Validator {
+    /**
+     * The request to get the parameter value from
+     */
     public Request $req;
+
     public SchemaValidator $validator;
 
+    /**
+     * constructs the object.
+     * @param Request $req the request to get the parameter values from
+     */
     public function __construct(Request $req)
     {
         $this->req = $req;
-        $validator = new SchemaValidator(SchemaValidator::VALIDATE_AS_REQUEST);
+        $this->validator = new SchemaValidator(SchemaValidator::VALIDATE_AS_REQUEST);
     }
 
+    /**
+     * validate an input parameter
+     * @param string $name the name of the parameter
+     * @param string $in where the parameter should be found ("path","query","header","cookie")
+     * @param bool $required whether this parameter is required or not
+     * @param bool $allowEmpty CURRENTLY IGNORED | (query only) if the parameter can be empty
+     * @param bool $allowReserved CURRENTLY IGNORED | (query only) whether the parameter value SHOULD allow reserved characters, as defined by RFC3986 :/?#[]@!$&'()*+,;= to be included without percent-encoding.
+     * @param string $oas3Schema the parameter schema as JSON string
+     */
     public function validateParameter(string $name, string $in, bool $required, bool $allowEmpty, bool $allowReserved, string $oas3Schema){
         if($in == "path"){
             return $this->validateURLParameter($name,$oas3Schema,$required);
@@ -45,6 +65,9 @@ abstract class OAS3Validator {
         return $this->validateAgainstSchema($oas3Schema,$value);
     }
 
+    /**
+     * $allowEmpty & $allowReserved are currently ignored!
+     */
     public function validateQueryParameter(string $name, string $oas3Schema, bool $required, bool $allowEmpty, bool $allowReserved):bool{
         if($required == false && !isset($this->req->queryParameters[$name])){
             return true;
@@ -82,6 +105,11 @@ abstract class OAS3Validator {
         return $this->validateAgainstSchema($oas3Schema,$value);
     }
 
+    /**
+     * validates a value against an oas3 schema
+     * @param string $oas3Schema the schema as json string
+     * @param mixed $value the value to check
+     */
     public function validateAgainstSchema(string $oas3Schema, mixed $value):bool{
         $spec = Reader::readFromJson($oas3Schema);
         $schema = new Schema($spec->schema);
@@ -94,9 +122,16 @@ abstract class OAS3Validator {
     }
 
 
+    /**
+     * this function should be implemented by any inheritant of this class
+     * (the code generator will automatically implement this function when creating validation classes)
+     */
     public abstract function validateRequest():OAS3ValidationResult;
 }
 
+/**
+ * a class representing a result of a parameter validation against an oas3 schema
+ */
 class OAS3ValidationResult{
     public bool $validationSuccess;
     public string $errorMessage;
--- src/LUAPI/OAS3/csfixer-config.php
+++ src/LUAPI/OAS3/csfixer-config.php
@@ -1,4 +1,7 @@
 <?php
+/**
+ * cs-fixer config for code generator. you can change the file if you need to.
+ */
 $cfg = new PHpCsFixer\Config();
 $cfg->setRules([
     '@PSR2' => true,
